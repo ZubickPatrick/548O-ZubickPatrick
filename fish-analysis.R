67d1f64f8e7661ea0962df279003205942e5f162
ZubickPatrick <zubickp@gmail.com>
2021-11-09 15:59:08 -0800

    Annotating code for exercise 1 and send to GIT. Added a column to dataframe for fish size @ >200, and then altered the size cutoff to >300.

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,5 +1,7 @@
 fish_data = read.csv("Gaeta_etal_CLC_data.csv") 
 #chANGE
 library(dplyr)
+#create new categorical size column for dataframe fish_data
 fish_data_cat = fish_data %>% mutate(length_cat = ifelse(length > 200, "big", "small"))
+#change the size of the category cutoff from > 200 to >300
 fish_data_cat = fish_data %>% mutate(length_cat = ifelse(length > 300, "big", "small"))
