e22d38d8221c7e53593329f628a67ac367e716c1
ZubickPatrick <zubickp@gmail.com>
2021-11-09 16:06:10 -0800

    rename Gaeta_etal data file manually in R and then changed first line of fish-analysis script to reflect the change.

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,7 +1,7 @@
-fish_data = read.csv("Gaeta_etal_CLC_data.csv") 
-#chANGE
+fish_data = read.csv("Gaeta_etal_CLC_data_1.csv")
 library(dplyr)
 #create new categorical size column for dataframe fish_data
 fish_data_cat = fish_data %>% mutate(length_cat = ifelse(length > 200, "big", "small"))
 #change the size of the category cutoff from > 200 to >300
 fish_data_cat = fish_data %>% mutate(length_cat = ifelse(length > 300, "big", "small"))
+fish_data = read.csv("data/Gaeta_etal_CLC_data.csv")
