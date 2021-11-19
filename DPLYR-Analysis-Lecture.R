952ea91485647870b29d219caace6447ab574b92
ZubickPatrick <zubickp@gmail.com>
2021-11-18 16:38:55 -0800

    Exercise 2 P1 upto the piping part is done. Will complete over the weekend.
    Cheers patty, you feel like a wizard.

--- DPLYR-Analysis-Lecture.R
+++ DPLYR-Analysis-Lecture.R
@@ -45,3 +45,66 @@ view(filtered_shrub)
 shrub_data_w_vols = mutate(shrub_data, volume = length * width * height)
 view(shrub_data_w_vols)
 
+#starting exercise 2
+
+#learning about aggregation
+
+group_by(surveys, species_id)
+
+surveys_by_species = group_by(surveys, species_id)
+
+# calculate some values for each group 
+
+summarize(surveys_by_species, abundance = n())
+
+# group by multiple columns
+
+surveys_by_species_plot <- group_by(surveys, species_id, plot_id)
+summarize(surveys_by_species_plot, abundance = n())
+
+species_weight = summarize(surveys_by_species, avg_weight = mean(weight))
+
+view(species_weight)
+
+#remove na
+species_weight = summarize(surveys_by_species, avg_weight = mean(weight, na.rm = TRUE))
+
+#na didnt go away, need to omit it
+
+na.omit(species_weight)
+
+ds_data = filter(surveys, species_id == "DS")
+
+ds_data_by_year = group_by (ds_data, year)
+
+ds_weight_by_year = summarize(ds_data_by_year, avg_weight = mean(weight, na.rm = TRUE))
+
+# this was inefficient because of doing this in this way is a 3 step process. involving creating 3 different objects.
+
+# lets do exercise 2
+
+survey_year_month_day = select(surveys, year, month, day, species_id)
+view(survey_year_month_day)
+
+weight_kg = na.omit(surveys)
+weight_kg = select(weight_kg,year, species_id, weight)
+weight_kg = mutate(weight_kg, weight = weight / 1000)
+
+df_filter_SH = filter(surveys,species_id == "SH")
+view(df_filter_SH)
+
+survey_countspp= group_by(surveys,species_id)
+survey_countspp = summarize(survey_countspp, abundance = n())
+view(survey_countspp)
+
+surveys_spp_yr = group_by(surveys, year, species_id)
+surveys_spp_yr = summarize(surveys_spp_yr, abundance = n())
+view(surveys_spp_yr)
+
+surveys_mass = group_by(surveys, species_id)
+surveys_mass = filter(surveys_mass, species_id =="DO")
+surveys_mass = na.omit(surveys_mass)
+surveys_mass = summarize(surveys_mass, mean_weight = mean(weight))
+
+
+
