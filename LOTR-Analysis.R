0824dba570827891ca3c199e58cfad2a287d93a7
ZubickPatrick <93403774+ZubickPatrick@users.noreply.github.com>
2021-11-19 12:36:47 -0800

    Finished workshop exercises from lecture.

--- LOTR-Analysis.R
+++ LOTR-Analysis.R
@@ -21,6 +21,7 @@ Male = read_csv("data/Male.csv")
 # need to add the two together
 lotr_count_untidy = bind_rows( Male, Female)
 str(lotr_count_untidy)
+lotr_count_untidy
 #save the new untidy count dataframe to computer
 write_csv(lotr_count_untidy, "data/lotr_count_untidy.csv")
 # still need to tidy this up by adding the races to one column with word count in another.
@@ -31,8 +32,7 @@ lotr_count_tidy
 write_csv(lotr_count_tidy, "data/lotr_count_tidy.csv")
 # find total number of words by race across entire trilogy. 
 # words spoken by males hobitts can be found here
-lotr_tidy %>% 
-  +     count(Gender, Race, wt = Words)
+lotr_tidy %>% count(Gender, Race, wt = Words)
 # words spoken by race and gender is done here
 (by_race_film <- lotr_tidy %>% 
     group_by(Film, Race) %>% 
@@ -47,7 +47,49 @@ lotr_untidy_counting = mutate(lotr_untidy,words = Female + Male)
 # do a plot here too for the last step. 
 p2 = ggplot(lotr_untidy_counting, aes(x=Film, y = words, fill = Race))
 p2 + geom_bar(stat= "identity", position = "dodge")+coord_flip() + guides(fill = guide_legend(reverse = TRUE))
-#need to do this last analysis using the 3 basic individual dataframes.
+#need to do this last analysis using the 3 basic individual film dataframes.
+
+#Mutate and add both sex columns to make a new one called word_race to each movie species DF
+
+Wordcountracefellowship = mutate(The_Fellowship_Of_The_Ring, Words_race = Female + Male)
+Wordcountracefellowship
+
+Wordcountracereturn = mutate(The_Return_Of_The_King, Words_race = Female + Male)
+Wordcountracereturn
+
+Wordcountracetowers = mutate(The_Two_Towers, Words_race = Female + Male)
+Wordcountracetowers
+
+#lets add these 3 DF together.
+lotr_longway <- bind_rows(Wordcountracereturn,Wordcountracefellowship, Wordcountracetowers)
+
+group_lotr_long =  group_by(lotr_longway, Race)
+
+new_lotr_longway  = dplyr::select(group_lotr_long, Film, Race, Words_race)
+new_lotr_longway
+
+# answer for the number of words spoken by race across entire trilogy is. elfs speak 2200,693 and 844, hobbit speaks 3658,2675, 2463, man speaks, 1995,2727, 3990  respectively.
+# i guess we can try to add them all up for race across the whole trilogy. 
+
+new_lotr_longway %>% group_by(Race) 
+new_lotr_longway %>% group_by(Race,Film)
+
+#pull out individual race values
+
+Elf_longway = dplyr::filter(new_lotr_longway, Race == "Elf")
+summarise(Elf_longway, Words_race)
+sum(Elf_longway$Words_race)
+
+Man_longway = dplyr::filter(new_lotr_longway, Race == "Man")
+summarise(Man_longway, Words_race)
+sum(Man_longway$Words_race)
+
+Hob_longway = dplyr::filter(new_lotr_longway, Race == "Hobbit")
+summarise(Hob_longway, Words_race)
+sum(Hob_longway$Words_race)
+
+# now can see that the full trilogy values for hobbit = 8796, man = 8712, elf = 3737.
+
 
   
 
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Repository for all of my work in BIOL-548O
 ##Hello Patrick!!
 #thanks for doing this with me Elsa!
 # lets see how well this works on my home computer.
+# appears to work so were good to go!
