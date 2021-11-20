4ea41d15186e1771ce32c560d8bd3689ca8e1747
ZubickPatrick <93403774+ZubickPatrick@users.noreply.github.com>
2021-11-20 12:48:04 -0800

    verified completion of this workshop.

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,10 +1,23 @@
-fish_data = read.csv("Gaeta_etal_CLC_data_1.csv")
+#this code takes gaeta et al fish data and adds a new categorical variable for length greater 200 = big or small, then alters that cataegory.
+
+
+fish_data = read.csv("data/Gaeta_etal_CLC_data_1.csv")
 library(dplyr)
+
 #create new categorical size column for dataframe fish_data
+
 fish_data_cat = fish_data %>% mutate(length_cat = ifelse(length > 200, "big", "small"))
+fish_data_cat
+
 #change the size of the category cutoff from > 200 to >300
+
 fish_data_cat = fish_data %>% mutate(length_cat = ifelse(length > 300, "big", "small"))
-fish_data = read.csv("data/Gaeta_etal_CLC_data.csv")
+
+fish_data = read.csv("data/Gaeta_etal_CLC_data_1.csv")
+
 #used R as a time machine and deleted + reverted a few different things.
-fish_data_cat = fish_data %>% 
-  mutate(length_cat = ifelse(length > 300, "large", "small"))
+
+fish_data_cat = fish_data %>% mutate(length_cat = ifelse(length > 300, "large", "small"))
+fish_data_cat
+
+#did the partenr exercise with Elsa and i made changes to the md file of mslein
