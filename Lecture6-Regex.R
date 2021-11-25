a991fe5894fa010da17f896643c2edb002bd635d
ZubickPatrick <zubickp@gmail.com>
2021-11-25 15:49:00 -0800

    first exercise done

--- Lecture6-Regex.R
+++ Lecture6-Regex.R
@@ -2,3 +2,36 @@ library(stringr)
 install.packages("gapminder")
 gDat <- gapminder::gapminder
 str(gDat)
+view(gDat)
+gDat
+library(dplyr)
+library(tidyverse)
+view(gDat)
+
+# lets start this lecture 
+
+grep('\'', levels(gDat$country), value = TRUE)
+
+print("a\nb")
+cat("a\nb")
+
+(strings <- c("a", "ab", "acb", "accb", "acccb", "accccb"))
+
+grep("ac*b", strings, value = TRUE)
+
+grep("ac+b", strings, value = TRUE)
+
+grep("ac?b", strings, value = TRUE)
+
+grep("ac{2}b", strings, value = TRUE)
+
+grep("ac{2,}b", strings, value = TRUE)
+
+grep("ac{2,3}b", strings, value = TRUE)
+
+#Exercise 1:
+
+grep("ee+", gDat$country, value = TRUE) 
+# greece
+
+     
