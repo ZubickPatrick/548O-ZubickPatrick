26a48ff565d102c2bf2775c80388c125463785a2
ZubickPatrick <zubickp@gmail.com>
2021-11-25 16:32:53 -0800

    done exercise 3 YAYYYYYYY. FEEL like a wizard!

--- Lecture6-Regex.R
+++ Lecture6-Regex.R
@@ -53,4 +53,22 @@ grep("^ab", strings, value = TRUE)
 grep("ab$", strings, value = TRUE)
 grep("\\bab", strings, value = TRUE)
 
+(strings <- c("^ab", "ab", "abc", "abd", "abe", "ab 12"))
+grep("ab.", strings, value = TRUE)
+grep("ab[c-e]", strings, value = TRUE)
+grep("ab[^c]", strings, value = TRUE)
+grep("^ab", strings, value = TRUE)
+grep("\\^ab", strings, value = TRUE)
+grep("abc|abd", strings, value = TRUE)
+gsub("(ab) 12", "\\1 34", strings)
 
+
+#Exercise 3 countries with i or t and ends with land.
+
+itcountries = grep("i|t", gDat$country, value = TRUE)
+itcountries
+
+view(itcountries)
+landcountries = grep("land$", itcountries, value = TRUE)
+itLANDcountries = gsub("land", "LAND",landcountries)
+view(itLANDcountries)
