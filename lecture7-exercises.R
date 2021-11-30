147f8761b81e67a81a6ed2746547591eada47cb9
ZubickPatrick <zubickp@gmail.com>
2021-11-30 13:25:26 -0800

    exercise 2 complete

--- lecture7-exercises.R
+++ lecture7-exercises.R
@@ -15,6 +15,8 @@ calc_shrub_vol(0.8, 1.6, 2.0)
 shrub_vol <- calc_shrub_vol(0.8, 1.6, 2.0)
 shrub_vol
 
+#exercise 1
+
 x=3.75
 fact = 0.0022046
 calc_grams <- function(lb, fact) {
@@ -23,7 +25,30 @@ calc_grams <- function(lb, fact) {
 
 calc_grams(x,fact)
 
+#exercise 2
+
+#the below function estimates the mass in KG of an organism based on its length in meters for a set of values.
+
+get_mass_from_length_theropoda <- function(length){
+  mass <- 0.73 * length ^ 3.63
+  return(mass)
+}
+
+length = 16
+
+get_mass_from_length_theropoda(length)
 
 
+# new try of this function with getmassfromlength
+
+a=214.44
+b=1.46
+length = 26
 
 
+get_mass_from_length = function(length){
+  mass = a * length^ b
+return(mass)}
+get_mass_from_length(length)
+
+#done exercise 2
