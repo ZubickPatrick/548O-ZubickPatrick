725aa6fc70cbe9410ed04e37eb67779207fc3f2b
ZubickPatrick <zubickp@gmail.com>
2021-11-30 13:02:11 -0800

    first commit for lecture 7. Exercise 1 complete

---
+++ lecture7-exercises.R
@@ -0,0 +1,29 @@
+# lecture 7 workshop 
+
+{a = 2 
+b = 3 
+a + b}
+
+
+calc_shrub_vol <- function(length, width, height) {
+  volume <- length * width * height
+  return(volume)
+}
+
+calc_shrub_vol(0.8, 1.6, 2.0)
+
+shrub_vol <- calc_shrub_vol(0.8, 1.6, 2.0)
+shrub_vol
+
+x=3.75
+fact = 0.0022046
+calc_grams <- function(lb, fact) {
+  grams <- lb/fact
+  return(grams)}
+
+calc_grams(x,fact)
+
+
+
+
+
