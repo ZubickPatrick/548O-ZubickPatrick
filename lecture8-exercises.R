83b1b0d5ad1ef507c423ffb520b9dccf375ce818
ZubickPatrick <zubickp@gmail.com>
2021-12-02 15:37:03 -0800

    Exercise 2 complete

--- lecture8-exercises.R
+++ lecture8-exercises.R
@@ -38,3 +38,31 @@ w > 10
 x > y 
 
 masses > 40
+
+
+#exercise 2 
+x <- 6
+if (x > 5){
+  x <- x^2
+}
+x
+
+x <- 4
+if (x > 5){
+  x <- x^2
+}
+x
+
+
+veg_type <- "tree"
+volume <- 16.08
+if (veg_type == "tree") {
+  mass <- 2.65 * volume^0.9
+}
+mass
+
+age_class = "sapling"
+if (age_class=="sapling"){y = 10
+  
+}
+y
