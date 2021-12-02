91d13dc242d318db12dfe4561051fab446ef3eec
ZubickPatrick <zubickp@gmail.com>
2021-12-02 15:31:31 -0800

    First commit workshop 8. Exercise 1 complete

---
+++ lecture8-exercises.R
@@ -0,0 +1,40 @@
+# starting the last lecture workshop
+
+10 > 5
+"aang" == "aang"
+3 != 3
+"dog" %in% c("cat", "dog", "rabbit")
+
+5 > 2 & 6 >=10
+5 > 2 | 6 >=10
+
+c(1, 1, 2, 3, 1) == 1
+
+
+site = c('a', 'b', 'c', 'd')
+state = c('FL', 'FL', 'GA', 'AL')
+state == 'FL'
+site[state == 'FL']
+site[c(TRUE, TRUE, FALSE, FALSE)]
+
+
+library(tidyverse)
+
+w <- 10.2
+x <- 1.3
+y <- 2.8
+z <- 17.5
+colors <- c("red", "blue", "green")
+masses <- c(45.2, 36.1, 27.8, 81.6, 42.4)
+dna1 <- "attattaggaccaca"
+dna2 <- "attattaggaacaca"
+
+
+#Exercise 1
+w > 10 
+
+"green" %in% colors
+
+x > y 
+
+masses > 40
